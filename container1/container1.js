8eb034368f4f4444a0254f0a66dc761134eef9be
mdp <mh375200@dal.ca>
2025-03-18 13:19:41 -0300

    initial commit for container1 code

---
+++ container1/container1.js
@@ -0,0 +1,96 @@
+const express = require('express');
+const fs = require('fs'); 
+const axios = require('axios'); 
+const path = require('path');
+
+const app = express();
+const port = 6000;
+
+app.use(express.json()); // Parse JSON request bodies
+
+const FILE_DIRECTORY = "/mihir_PV_dir";
+
+const fileExists = async (file) => {
+  try {
+      await fs.access(path.join(FILE_DIRECTORY, file));
+      return true;
+  } catch {
+      return false;
+  }
+};
+
+fileExists("file.dat").then(exists => console.log("File exists:", exists));
+
+app.post('/store-file', async (req, res) => {
+  try {
+      const { file, data } = req.body;
+
+      if (!file) {
+          return res.status(400).json({ file: null, error: "Invalid JSON input." });
+      }
+      if (data === undefined || data === null) {
+          return res.status(400).json({ file: file, error: "Missing 'data' in JSON input." });
+      }
+
+      console.log('Received data:', data);
+      const filePath = path.join(FILE_DIRECTORY, file);
+      console.log('Attempting to write to file path:', filePath);
+
+      let newData = data?.replaceAll(" ", "") || data;
+
+      fs.writeFile(filePath, newData,(err) => {
+        if(err) {
+          return res.status(500).json({ file: req.body ? req.body.file : null, error: "Error while storing the file to the storage." });
+        }
+
+        return res.status(200).json({ file: file, message: "Success." });
+      });
+
+      
+
+  } catch (error) {
+      console.error('Error storing file:', error);
+      res.status(500).json({ file: req.body ? req.body.file : null, error: "Error while storing the file to the storage." });
+  }
+});
+
+app.post('/calculate', async (req, res) => {
+  try {
+    // 1. Validate input JSON
+    const { file, product } = req.body;
+    if (!file) {
+      return res.status(400).json({
+        file: file,
+        error: "Invalid JSON input."
+        });
+    }
+
+    const filePath = path.join('/mihir_PV_dir', file);
+    if (!fs.existsSync(filePath)) {
+        //console.log("File not found.. form console..")
+        return res.status(404).json({
+          error: "File not found.",
+          file: file
+        });
+      }
+
+    const response2 = await axios.post('http://c2-service:8000/process', { 
+      file: file,
+      product: product 
+    }); 
+
+    if(response2.status == 200) {
+
+      // 4. Return response from container 2
+      res.json(response2.data);
+    }
+
+  } catch (error) {
+    console.error('Error:', error);
+    res.status(error.status).json(error.response);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
+});
